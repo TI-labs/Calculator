d64e91054041dbc1000ac0daf329e0cb7f0241e7
Tidjani Stefel <tidjani.stefel@outlook.com>
2022-08-08 00:43:55 +0200

    Créer la structure html
    J'ai créer les fichier html css et js, mais aussi créer,
    la structure html(bouttons, écrans ...) en y ajoutant classes et ids.

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+function add(a,b){
+
+    return a+b
+
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+
+function divide(a,b){
+    return a/b
+}
+
+function operate(operator,n1,n2){
+    return operator(n1,n2);
+}
+
