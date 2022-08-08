c3a173b6543958a40a3a0aef1b08a711cd463e19
Tidjani Stefel <tidjani.stefel@outlook.com>
2022-08-09 01:17:46 +0200

    Créé la fonction operate

--- script.js
+++ script.js
@@ -14,21 +14,46 @@ function multiply(a,b){
     return a*b
 }
 
-
 function divide(a,b){
     return a/b
 }
 
-function operate(operator,n1,n2){
-    return operator(n1,n2);
-}
+let operatorsFunction = [add,subtract,multiply,divide];
 
+
+function operate(wantedOperator,n1,n2){
+   let chooseOperator = operatorsFunction.filter(operator => {
+    if(operator.name == wantedOperator){
+        return true;
+    }else{
+        return false;
+    }
+   })
+   return chooseOperator[0](n1,n2);
+  }
+
+
+let displayContainer = document.querySelector('#displayContainer');
 
 function populateDisplay(string){
-
-    let displayContainer = document.querySelector('#displayContainer');
     
     displayContainer.textContent += string;
 
 }
+
+function clearDisplay(){
+    displayContainer.textContent = '';
+
+}
+
+
+let digits = document.querySelectorAll('.digit');
+digits.forEach(digit => digit.addEventListener('click',(e) =>{
+    populateDisplay(digit.textContent);
+}))
+
+let operators = document.querySelectorAll('.operator');
+
+operators.forEach(operator => operator.addEventListener('click',(e) => {
+    
 
